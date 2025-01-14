750fde2fa8066766ae4717870b0043bc79ee978a
iheb <iheb.j@xgol.pro>
2025-01-14 18:22:14 +0000

    my first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { EmployeesComponent } from './components/employees/employees.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [CommonModule, HeaderComponent, FooterComponent, EmployeesComponent],
+  template: `
+    <app-header></app-header>
+    <main class="container mt-4">
+      <app-employees></app-employees>
+    </main>
+    <app-footer></app-footer>
+  `,
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'datahorizon-Iheb-Jendoubi';
-}
+export class AppComponent {}
